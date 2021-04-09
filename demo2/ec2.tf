2224f259796507065629b669df7eab18a381f8f8
Jerry Hall <hall.jerry9@gmail.com>
2021-04-08 23:37:05 -0700

    Add ssh comment

--- demo2/ec2.tf
+++ demo2/ec2.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "bcs_hacks_ec2_instance" {
   ami                     = data.aws_ami.ubuntu.id
   instance_type           = "t2.micro"    
   user_data               = file("user_data.sh")
+  # Note: You must create your own SSH key in AWS in order to SSH in to the instance
   key_name                = "jhall-dev-env"
   subnet_id               = aws_subnet.bcs_hacks_demo_subnet.id
   vpc_security_group_ids  = [aws_security_group.BCS_Hacks_sg.id]
